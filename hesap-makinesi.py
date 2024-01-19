bb0f24c761d3f3d499cbe4c39cb6d3a5bc3e244d
Celal AKSU <celalaksu@gmail.com>
2024-01-19 13:59:31 +0300

    hesap makinesi fonksiyonlar ile yazıldı

--- hesap-makinesi.py
+++ hesap-makinesi.py
@@ -1,5 +1,5 @@
-while True :
-
+# menü
+def menu():
     # Yapılacak işlemlerin çıktısı
     print("HESAP MAKİNESİ PROGRAMI")
     print("-----------------------")
@@ -15,54 +15,92 @@ while True :
     print(menü)
     print("-----------------------")
 
-    # Kullanıcının seçim yapması
-    secim = input("--> Yapmak istediğiniz işlemin numarasını yazınız : ")
-    if secim != "1" and secim != "2" and secim != "3" and secim != "4" :
-        print("Yanlış seçim yaptınız, lütfen kontrol ediniz.!!!")
-    else :
-        # Kullanıcıdan verilerin alınması
-        print("-----------------------")
-        print("Veri Girişi")
-        veriler = []
-        i = 1
-        while True:
-            veri = input(f"{i}. sayıyı giriniz ( Veri girişini tamamlamak için 'ç' harfine basınız!): ")
-            if veri == "ç" or veri == "Ç" :
-                break
-            else :
-                veri = int(veri)
-                veriler.append(veri)
-            i = i + 1
-        print("-----------------------")
-        print(f"Girişini yaptığınız veriler : {veriler}")
-        print("-----------------------")
-        # işlemler
-        if secim == "1" :
-            sonuc = 0 # sonuc = veriler[1]
-            for sayi in veriler :
-                sonuc = sonuc + sayi
-        elif secim == "2" :
-            sonuc = veriler[0]
-            for index_no in range(1, len(veriler)) :
-                sonuc = sonuc - veriler[index_no]
-        elif secim == "3" :
-            sonuc = 1 # sonuc = veriler[1]
-            for sayi in veriler :
-                sonuc = sonuc * sayi
-        elif secim == "4" :
-            sonuc = veriler[0]
-            for index_no in range(1,len(veriler)):
-                sonuc = sonuc // veriler[index_no]
+# kullanıcı seçimi
+def islem_secme():
+    secim = input("Yapmak istediğiniz işlemin numarasını giriniz :")
+    return secim
+
+# veri girişi
+def veri_girisi():
+    # Kullanıcıdan verilerin alınması
+    print("-----------------------")
+    print("Veri Girişi")
+    veriler = []
+    i = 1
+    while True:
+        veri = input(f"{i}. sayıyı giriniz ( Veri girişini tamamlamak için 'ç' harfine basınız!): ")
+        if veri == "ç" or veri == "Ç" :
+            break
         else :
-            print("Yanlış seçim yaptınız, lütfen kontrol ediniz.!!!")
-        # Çıktılar
-        print(f"İşleminizin sonucu = {sonuc}")
-        # print("Program kapatılıyor")
-    
-    print()
-    print("============================================")
-    secim = input("Devam etmek istiyor musunuz ? (E/H) : ")
-    if secim == "H" or secim == "h":
-        print("Program kapatılıyor")
-        print()
-        break
+            veri = int(veri)
+            veriler.appenvalueerror:d(veri)
+        i = i + 1
+    print("-----------------------")
+    return veriler
+    # girilen veriler kontrol edilmiyor
+    # hata denetimi
+
+# toplama
+def toplama(veriler):
+    sonuc = 0
+    for sayi in veriler :
+        sonuc = sonuc + sayi
+    print("toplama sonucu",sonuc)
+
+# çıkarma
+def cikarma(veriler):
+    sonuc = veriler[0]
+    for index_no in range(1, len(veriler)) :
+        sonuc = sonuc - veriler[index_no]
+    print(sonuc)
+
+# çarpma
+def carpma(veriler):
+    sonuc = 1 # sonuc = veriler[1]
+    for sayi in veriler :
+        sonuc = sonuc * sayi
+    print(sonuc)
+
+# bölme
+def bolme(veriler):
+    sonuc = veriler[0]
+    for index_no in range(1,len(veriler)):
+        sonuc = sonuc // veriler[index_no]
+    print(sonuc)
+
+def us_alma(veriler):
+    pass
+
+# çıkış
+def cikis():
+    print("==============================")
+    print("Programdan çıkış yapılıryor...")
+    exit()
+
+# ana fonksiyon
+def ana_fonksiyon():
+    while True :
+        menu()
+        secim = islem_secme()
+        print(f"Seçmiş olduğunuz işlem numarası : {secim}")
+        if secim == "6" or secim == "Ç" or secim == "ç":
+            cikis()
+        else:
+            veriler =  veri_girisi()
+        print(veriler)
+
+        match secim :
+            case "1" :
+                toplama(veriler)
+            case "2" :
+                cikarma(veriler)
+            case "3" :
+                carpma(veriler)
+            case "4" :
+                bolme(veriler)
+            case "5" :
+                us_alma(veriler)
+            case "6" :
+                cikis()
+
+ana_fonksiyon()
